88a156292ff6b0e6da8762f948a7d6a5efeae51e
Rita <shaidrv@gmail.com>
2023-01-13 18:17:59 +0300

    добавила 3 компонента и роутер

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Tasks from '../views/Tasks'
+import New from '../views/New'
+import Task from '../views/New'
+
+const routes = [
+  {
+    path: '/',
+    name: 'tasks',
+    component: Tasks,
+  },
+  {
+    path: '/new',
+    name: 'new',
+    component: New,
+  },
+  {
+    path: '/task',
+    name: 'task',
+    component: Task,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active',
+})
+
+export default router
