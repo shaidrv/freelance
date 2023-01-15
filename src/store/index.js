d220f9587b21f07c1da838c3ea243a771bcd70ec
Rita <shaidrv@gmail.com>
2023-01-15 13:37:20 +0300

    создала store и сделала главную страницу
    вывела список задач на главную, количество активных задач. в store создала мутацию и экшн 'создать задачу'

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,25 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    tasks: [],
   },
   getters: {
+    tasks(state) {
+      return state.tasks
+    },
+    activeTasksCount(state) {
+      return state.tasks.filter((task) => task.status === 'active').length
+    },
   },
   mutations: {
+    createTask(state, task) {
+      return state.tasks.push(task)
+    },
   },
   actions: {
+    createTask({ commit }, task) {
+      commit('createTask', task)
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,16 +1,16 @@
 <template>
-  <h1 class="text-white center">Задач пока нет</h1>
-  <template >
-    <h3 class="text-white">Всего активных задач: 0</h3>
-    <div class="card">
+  <h1 class="text-white center" v-if="tasks.length === 0">Задач пока нет</h1>
+  <template v-else>
+    <h3 class="text-white">Всего активных задач: {{ activeTasksCount }}</h3>
+    <div class="card" v-for="task in tasks" :key="task.id">
       <h2 class="card-title">
-        Название задачи
-        <AppStatus :type="'done'" />
+        {{ task.title }}
+        <AppStatus :type="task.status" />
       </h2>
       <p>
         <strong>
           <small>
-            {{new Date().toLocaleDateString()}}
+            {{ new Date(task.date).toLocaleDateString() }}
           </small>
         </strong>
       </p>
@@ -23,6 +23,14 @@
 import AppStatus from '../components/AppStatus'
 
 export default {
-  components: {AppStatus}
+  computed: {
+    tasks() {
+      return this.$store.getters.tasks
+    },
+    activeTasksCount() {
+      return this.$store.getters.activeTasksCount
+    },
+  },
+  components: { AppStatus },
 }
 </script>
