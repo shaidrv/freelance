548790407b8c5d3ece38cc2befcce7258510b81c
Rita <shaidrv@gmail.com>
2023-01-17 20:13:49 +0300

    Динамически вывела название статуса и добавила динамические стили

--- src/components/AppStatus.vue
+++ src/components/AppStatus.vue
@@ -1,3 +1,40 @@
 <template>
-  <span :class="['badge']">Название статуса</span>
+  <span :class="['badge', badgeClass]">{{ title }}</span>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      titles: {
+        active: 'Активен',
+        done: 'Завершен',
+        cancelled: 'Отменен',
+        pending: 'Выполняется',
+      },
+      classes: {
+        active: 'primary',
+        done: 'primary',
+        cancelled: 'danger',
+        pending: 'warning',
+      },
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      validator(value) {
+        return ['active', 'done', 'cancelled', 'pending'].includes(value)
+      },
+    },
+  },
+  computed: {
+    title() {
+      return this.titles[this.type]
+    },
+    badgeClass() {
+      return this.classes[this.type]
+    },
+  },
+}
+</script>
